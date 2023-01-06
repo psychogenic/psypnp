f6ab73413a7928ec1e015b89cf48795c29035cd0
Pat Deegan <pat.devel@psychogenic.com>
2023-01-06 07:17:02 -0500

    Set user_config defaults more inline with existing behaviour of lib

--- lib/psypnp/user_config.py
+++ lib/psypnp/user_config.py
@@ -5,12 +5,8 @@
 # list any names of feeders to always leave untouched in reset part, e.g. permanent reels
 feeders_reset_skiplist = [
     
-    'ReelLeft8mm_01',
-    'ReelLeft8mm_02',
-    'ReelLeft8mm_04',
-    'ReelLeft8mm_05',
-    'ReelLeft8mm_06',
-    'ReelLeft8mm_07'
+    'ExampleReelLeft8mm_02',
+    'ExampleReelLeft8mm_07'
     
     ] 
 
@@ -18,5 +14,5 @@ feeders_reset_skiplist = [
 # auto-feed setup stuff
 autofeedsetup_allow_part_spreading = True # allow a part to be spread amongst multiple feed sets
 autofeedsetup_map_parts_to_preset_feeders = True # if a part is used in proj, and already mapped to feeder, leave it be 
-autofeedsetup_leave_already_assoc_feeds_untouched = True # leave all non "fiducial" or "home" feeders untouched
-autofeedsetup_restrict_to_enabled_feeders = True # only place in feeders that are enabled
+autofeedsetup_leave_already_assoc_feeds_untouched = False # leave all non "fiducial" or "home" feeders untouched
+autofeedsetup_restrict_to_enabled_feeders = False # only place in feeders that are enabled
